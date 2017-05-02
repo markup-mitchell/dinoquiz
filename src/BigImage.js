ca402a51ad962da18c08635395b54e7907aa3a6e
spitchell <mr.spitchell@gmail.com>
2017-05-02 16:32:38 +0100

    add node to eslint environment to avoid require not defined errors

--- src/BigImage.js
+++ src/BigImage.js
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types';
 function BigImage(props) {
   return (
     <div className='image-board'>
-      <img src={require(props.src)} alt={props.alt} />
+    <img src={require(props.src)} alt={props.alt} />
     </div>
   );
 }
@@ -13,4 +13,4 @@ BigImage.propTypes = {
   src: PropTypes.string.isRequired
 };
 
-module.exports = BigImage;
+export default BigImage;
